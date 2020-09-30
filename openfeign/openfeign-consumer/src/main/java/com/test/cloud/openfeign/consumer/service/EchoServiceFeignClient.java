740e950099b8d72808d36c88486085e98ffc5ae4
thbspan <thbspan@163.com>
2020-09-30 11:05:29 +0800

    feign java config

--- openfeign/openfeign-consumer/src/main/java/com/test/cloud/openfeign/consumer/service/EchoServiceFeignClient.java
+++ openfeign/openfeign-consumer/src/main/java/com/test/cloud/openfeign/consumer/service/EchoServiceFeignClient.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "openfeign-provider-demo")
+import com.test.cloud.openfeign.consumer.config.OpenfeignProviderDemoConfig;
+
+@FeignClient(name = "openfeign-provider-demo", configuration = OpenfeignProviderDemoConfig.class)
 public interface EchoServiceFeignClient {
 
     @GetMapping("/echo")
