12af033b05255fbf1385f8882659a2f989b29fa3
thbspan <thbspan@163.com>
2020-10-04 10:22:18 +0800

    add config refresh by bus example

--- config/config-user-application/src/main/java/com/test/cloud/config/user/controller/DemoController.java
+++ config/config-user-application/src/main/java/com/test/cloud/config/user/controller/DemoController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.test.cloud.config.user.config.OrderProperties;
 
 @RestController
 @RequestMapping("/demo")
+@RefreshScope
 public class DemoController {
     @Autowired
     private OrderProperties orderProperties;
