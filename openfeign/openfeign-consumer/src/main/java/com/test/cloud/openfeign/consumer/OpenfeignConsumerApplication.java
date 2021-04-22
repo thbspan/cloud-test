b8fd179b28940d807c0f83a8d54844a424d5940d
thbspan <thbspan@163.com>
2021-04-22 14:27:11 +0800

    fix openfeign run bug in spring-cloud 3.0

--- openfeign/openfeign-consumer/src/main/java/com/test/cloud/openfeign/consumer/OpenfeignConsumerApplication.java
+++ openfeign/openfeign-consumer/src/main/java/com/test/cloud/openfeign/consumer/OpenfeignConsumerApplication.java
@@ -2,7 +2,12 @@ package com.test.cloud.openfeign.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerProperties;
+import org.springframework.cloud.loadbalancer.blocking.client.BlockingLoadBalancerClient;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,5 +15,14 @@ public class OpenfeignConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OpenfeignConsumerApplication.class, args);
+    }
+
+    /**
+     * 强制使用 BlockingLoadBalancerClient 实例，也可以使用ExtendRibbonLoadBalancerClient
+     */
+    @Bean
+    public LoadBalancerClient blockingLoadBalancerClient(LoadBalancerClientFactory loadBalancerClientFactory,
+                                                         LoadBalancerProperties properties) {
+        return new BlockingLoadBalancerClient(loadBalancerClientFactory, properties);
     }
 }
