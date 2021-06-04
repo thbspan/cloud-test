a5079f29f8814bb0f081ff0002159326251b843c
thbspan <thbspan@163.com>
2021-06-04 17:22:09 +0800

    update springcloud config example

--- config/config-user-application/src/main/java/com/test/cloud/config/user/ConfigUserApplication.java
+++ config/config-user-application/src/main/java/com/test/cloud/config/user/ConfigUserApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigUserApplication {
     public static void main(String[] args) {
+        // 支持SpringCloud 2020.x.x
+        System.setProperty("spring.cloud.bootstrap.enabled", "true");
         SpringApplication.run(ConfigUserApplication.class, args);
     }
 }
