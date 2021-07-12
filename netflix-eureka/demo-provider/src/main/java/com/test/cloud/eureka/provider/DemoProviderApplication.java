afda295a8edd231c4c2bdf4b019ac441d6fd7fab
thbspan <thbspan@163.com>
2021-07-12 20:45:19 +0800

    update eureka client demo

--- netflix-eureka/demo-provider/src/main/java/com/test/cloud/eureka/provider/DemoProviderApplication.java
+++ netflix-eureka/demo-provider/src/main/java/com/test/cloud/eureka/provider/DemoProviderApplication.java
@@ -1,9 +1,11 @@
 package com.test.cloud.eureka.provider;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringCloudApplication
 public class DemoProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoProviderApplication.class, args);
