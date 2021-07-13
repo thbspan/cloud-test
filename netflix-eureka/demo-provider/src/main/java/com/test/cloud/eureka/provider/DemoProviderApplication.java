ddda4d6dc04ae63d30155279f033d53da178caca
thbspan <thbspan@163.com>
2021-07-13 19:28:35 +0800

    use web client client demo

--- netflix-eureka/demo-provider/src/main/java/com/test/cloud/eureka/provider/DemoProviderApplication.java
+++ netflix-eureka/demo-provider/src/main/java/com/test/cloud/eureka/provider/DemoProviderApplication.java
@@ -1,11 +1,11 @@
 package com.test.cloud.eureka.provider;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringCloudApplication
+@SpringBootApplication
 public class DemoProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoProviderApplication.class, args);
