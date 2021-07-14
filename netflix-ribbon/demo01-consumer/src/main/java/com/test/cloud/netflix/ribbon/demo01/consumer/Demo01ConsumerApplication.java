0040d12efe81d7b9e2b146726d049d5205d89061
thbspan <thbspan@163.com>
2021-07-14 19:07:21 +0800

    add ribbon config file demo

--- netflix-ribbon/demo01-consumer/src/main/java/com/test/cloud/netflix/ribbon/demo01/consumer/Demo01ConsumerApplication.java
+++ netflix-ribbon/demo01-consumer/src/main/java/com/test/cloud/netflix/ribbon/demo01/consumer/Demo01ConsumerApplication.java
@@ -1,11 +1,15 @@
 package com.test.cloud.netflix.ribbon.demo01.consumer;
 
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Demo01ConsumerApplication {
 
@@ -13,14 +17,17 @@ public class Demo01ConsumerApplication {
         SpringApplication.run(Demo01ConsumerApplication.class, args);
     }
 
+    /**
+     * 使用HttpComponents发送网络请求
+     */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault()));
     }
 
     @Bean("notLoadBalanceRestTemplate")
     public RestTemplate notLoadBalanceRestTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault()));
     }
 }
